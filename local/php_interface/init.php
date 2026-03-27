d8822d416e31de575a49f1f62b39b6405f5c99e8
Alexander <alexander@MacBook-Pro-Alexander.local>
2026-03-27 11:28:15 +0300

    Добавил подключения композер

---
+++ local/php_interface/init.php
@@ -0,0 +1,5 @@
+Создайте /local/php_interface/init.php со следующим содержимым
+<?php
+require_once __DIR__ . ’/../../vendor/autoload.php’;
+Для добавления пакетов используйте composer require vendor_name/package_name
+# (например, composer require symfony/var-dumper)
