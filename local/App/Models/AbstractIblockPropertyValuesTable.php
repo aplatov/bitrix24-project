114a33ce11639d713e89c180df3765ea96d7f3f9
Alexander <alexander@MacBook-Pro-Alexander.local>
2026-08-25 18:06:49 +0300

    ДЗ-3_1

---
+++ local/App/Models/AbstractIblockPropertyValuesTable.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+
+use Bitrix\Main\ORM\Data\DataManager;
+
+
+/**
+ * Class AbstractIblockPropertyValueTable
+ *
+ * @package Models
+ */
+abstract class AbstractIblockPropertyValuesTable extends DataManager {}
